6f4feffe578fb131d843f79ab1f42ff8d16dc01d
César Burgos <85316714+CesarBurgos@users.noreply.github.com>
2026-05-21 13:32:53 -0600

    update
    add new files

--- proyecto/src/Main.java
+++ proyecto/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //EnvLoader var_env = EnvLoader.get("DB_HOST");
 
     }
 }
---
+++ proyecto/src/util/APIConstants.java
@@ -0,0 +1,17 @@
+package util;
+
+public class APIConstants {
+    public static final Boolean OKSUCCESS = true;
+    public static final Boolean ERRORSUCCESS = false;
+
+    public static final String OKQUERY = "Consulta Correcta";
+    public static final String OKCREATE = "Registro Correcto";
+    public static final String OKUPDATE = "Registro Actualizado";
+    public static final String OKDELETE = "Registro Eliminado";
+
+    public static final String OKFOUND = "Registro encontrado";
+
+    public static final String ERRORQUERY = "Error en la consulta";
+    public static final String ERRORNOTFOUND = "Registro no encuentro";
+    public static final String ERRORGENERAL = "Ocurrió un error";
+}
